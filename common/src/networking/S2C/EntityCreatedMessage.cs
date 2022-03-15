9e8a53b9cfa7203fb235d0619f9c1522406bbfd5
capitalistspz <amponsahperez@gmail.com>
2022-03-15 17:06:51 +0000

    Implemented entity creation and removal messages

---
+++ common/src/networking/S2C/EntityCreatedMessage.cs
@@ -0,0 +1,50 @@
+using System;
+using common.entities;
+using LiteNetLib.Utils;
+using Microsoft.Xna.Framework;
+
+namespace common.networking.S2C
+{
+    public class EntityCreatedMessage : INetSerializable
+    {
+        private Guid _id;
+        private EntityType _entityType;
+        private Vector2 _entityPosition;
+        public EntityCreatedMessage(BaseEntity entity)
+        {
+            switch (entity)
+            {
+                case PlayerEntity:
+                    _entityType = EntityType.PlayerEntity;
+                    break;
+                default:
+                    _entityType = EntityType.Unknown;
+                    break;
+            }
+
+            _entityPosition = entity.Position;
+            _id = entity.Id;
+        }
+        public void Serialize(NetDataWriter writer)
+        {
+            writer.Put((byte)S2CMessageType.EntityCreated);
+            writer.Put((byte)_entityType);
+            writer.Put(_entityPosition.X);
+            writer.Put(_entityPosition.Y);
+            writer.Put(_id.ToByteArray());
+        }
+
+        public void Deserialize(NetDataReader reader)
+        {
+            _ = reader.GetByte();
+            _entityType = (EntityType) reader.GetByte();
+            _entityPosition.X = reader.GetFloat();
+            _entityPosition.Y = reader.GetFloat();
+            
+            // Guid is made of 16 bytes
+            var bytes = new byte[16];
+            reader.GetBytes(bytes, 16);
+            _id = new Guid(bytes);
+        }
+    }
+}
---
+++ common/src/networking/S2C/EntityRemovedMessage.cs
@@ -0,0 +1,29 @@
+using System;
+using LiteNetLib.Utils;
+
+namespace common.networking.S2C
+{
+    public class EntityRemovedMessage : INetSerializable
+    {
+        private Guid _id;
+
+        public EntityRemovedMessage(Guid removedEntityId)
+        {
+            _id = removedEntityId;
+        }
+
+        public void Serialize(NetDataWriter writer)
+        {
+            writer.Put((byte)S2CMessageType.EntityRemoved);
+            writer.Put(_id.ToByteArray());
+        }
+
+        public void Deserialize(NetDataReader reader)
+        {
+            _ = reader.GetByte();
+            var bytes = new byte[16];
+            reader.GetBytes(bytes, 16);
+            _id = new Guid(bytes);
+        }
+    }
+}
