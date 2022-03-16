2cb4094e5972debb16fd32ded2a8b70446bb6204
capitalistspz <amponsahperez@gmail.com>
2022-03-16 21:11:32 +0000

    Added command context where arguments values are stored as well as the game

---
+++ common/src/command/CommandContext.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using common.core;
+using Microsoft.Xna.Framework;
+
+namespace common.command
+{
+    public class CommandContext
+    {
+        private readonly Dictionary<string, string> _args;
+        private IGame _game;
+        public CommandContext(IGame game)
+        {
+            _game = game;
+            _args = new Dictionary<string, string>();
+        }
+
+        public string GetArg(string argName)
+        {
+            return _args[argName];
+        }
+
+        public void SetArg(string argName, string value)
+        {
+            _args[argName] = value;
+        }
+    }
+}
