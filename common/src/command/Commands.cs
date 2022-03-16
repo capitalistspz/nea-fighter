db64d63966d2077fb3807c536eaa70e84c7d31d9
capitalistspz <amponsahperez@gmail.com>
2022-03-16 21:16:27 +0000

    static commands class which holds all commands

---
+++ common/src/command/Commands.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+
+namespace common.command
+{
+    public static class Commands
+    {
+        private static CommandNode _commands;
+        private static Dictionary<string, string> _helpMessages;
+
+        static Commands()
+        {
+            _commands = new CommandNode("base", CommandInputType.Literal);
+            _helpMessages = new Dictionary<string, string>();
+        }
+        public static void AddCommand(CommandNode command)
+        {
+            _commands.Next(command);
+        }
+
+        public static void AddCommand(CommandNode command, string helpMessage)
+        {
+            _helpMessages.Add(command.GetName(), helpMessage);
+            _commands.Next(command);
+        }
+
+        public static CommandNode GetChild(string name)
+        {
+            return _commands.GetChild(name);
+        }
+
+        public static CommandNode GetRoot()
+        {
+            return _commands;
+        }
+    }
+}
