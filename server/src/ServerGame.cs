1840079a41ddc6a8ddb5cc8d9e7db5bc697b0f35
capitalistspz <amponsahperez@gmail.com>
2022-03-16 19:40:42 +0000

    ServerGame no longer creates player entities upon client connection, instead they are created when the client declares existence. Also, when a client connects, the client is sent the existing entities LocalPlayerUpdate response implemented Clients are sent movement updates when any client sends an input

--- server/src/ServerGame.cs
+++ server/src/ServerGame.cs
@@ -7,6 +7,7 @@ using LiteNetLib;
 using LiteNetLib.Utils;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.Tiled;
 using Serilog;
 using server.entities;
 
@@ -29,75 +30,135 @@ namespace server
 
         protected override void Initialize()
         {
-            
+            _graphics.PreferredBackBufferHeight = 10;
+            _graphics.PreferredBackBufferWidth = 10;
+            _graphics.ApplyChanges();
+            InitNetwork();
             base.Initialize();
         }
 
-        protected void InitNetwork()
+        private void InitNetwork()
         {
             _listener = new EventBasedNetListener();
             _server = new NetManager(_listener);
             _server.Start(IPAddress.Loopback, IPAddress.IPv6None, 35203);
             _listener.NetworkReceiveEvent += OnReceiveData;
             _listener.PeerConnectedEvent += OnConnect;
+            _listener.ConnectionRequestEvent += OnConnectRequest;
+            _listener.PeerDisconnectedEvent += OnDisconnect;
             _playerManager = new PlayerManager();
+            var tiledMap = Content.Load<TiledMap>("maps/default");
+            _world = new World(tiledMap);
+            Log.Information("Server initialised");
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
+                
             base.LoadContent();
         }
 
         protected override void Update(GameTime gameTime)
         {
+            _server.PollEvents();
+            _world.Update(gameTime);
             base.Update(gameTime);
         }
+        
+        private void OnConnectRequest(ConnectionRequest request)
+        {
+            Log.Information("Received a connection request from {RequestSource}", request.RemoteEndPoint);
+            request.Accept();
+        }
 
+        private void OnDisconnect(NetPeer peer, DisconnectInfo disconnectInfo)
+        {
+            Log.Information("User(s) at {Peer} disconnected, reason: {DisconnectReason}", peer.EndPoint, disconnectInfo.Reason);
+            var disconnected = _playerManager.GetPlayersByNetId(peer.Id);
+            foreach (var player in disconnected)
+            {
+                _world.RemoveEntity(player);
+                _playerManager.RemovePlayer(player);
+            }
+            
+        }
         private void OnConnect(NetPeer peer)
         {
-            var player = new ServerPlayerEntity(_world, Guid.NewGuid(), Vector2.Zero, 0);
-            var outMsg = new EntityCreatedMessage(player);
+            Log.Information("User connected from {PeerEndpoint}", peer.EndPoint);
+            
+            // Send all existing entities to the new peer
+            var entities = _world.GetAllEntities();
             var writer = new NetDataWriter();
-            writer.Put(outMsg);
-            _server.SendToAll(writer, DeliveryMethod.ReliableOrdered);
-            _playerManager.AddPlayer(player, peer.Id);
-            _world.AddEntity(player);
+            foreach (var entity in entities)
+            {
+                var msg = new EntityCreatedMessage(entity);
+                writer.Put(msg);
+                peer.Send(writer, DeliveryMethod.ReliableUnordered);
+                writer.Reset();
+            }
         }
 
         private void OnReceiveData(NetPeer peer, NetPacketReader reader, DeliveryMethod deliveryMethod)
         {
+            
             var type = (C2SMessageType) reader.PeekByte();
+            Log.Debug("Received data message of type {MsgType}", type);
             switch (type)
             {
                 case C2SMessageType.Input:
+                {
                     var inputMsg = reader.Get<InputMessage>();
                     var inputArgs = inputMsg.InputArgs;
                     var player = _playerManager.GetPlayer(peer.Id, inputArgs.LocalPlayerId);
                     player.ApplyInput(inputMsg.InputArgs);
+                    var writer = new NetDataWriter();
+                    writer.Put(new EntityMotionMessage(player));
+                    _server.SendToAll(writer, DeliveryMethod.ReliableSequenced);
+                }
                     break;
                 case C2SMessageType.ChatMessage:
                     break;
                 case C2SMessageType.LocalPlayerUpdate:
+                {
                     var localUpdateMsg = reader.Get<LocalPlayerUpdateMessage>();
                     if (!localUpdateMsg.WasRemoved)
                     {
-                        var newLocalPlayer = new ServerPlayerEntity(_world, Guid.NewGuid(), Vector2.Zero, localUpdateMsg.LocalPlayerId);
-                        _playerManager.AddPlayer(newLocalPlayer, peer.Id);
+                        var newLocalPlayer = new ServerPlayerEntity(_world, Guid.NewGuid(), _world.GetNewSpawn(), localUpdateMsg.LocalPlayerId, peer.Id);
                         
+                        _playerManager.AddPlayer(newLocalPlayer);
+                        _world.AddEntity(newLocalPlayer);
+                        
+                        var writer = new NetDataWriter();
+                        writer.Put(new EntityCreatedMessage(newLocalPlayer));
+                        _server.SendToAll(writer, DeliveryMethod.ReliableOrdered, peer);
+                        
+                        writer.Reset();
+                        writer.Put(new AssignIdMessage(newLocalPlayer));
+                        peer.Send(writer, DeliveryMethod.ReliableOrdered);
                     }
                     else
                     {
                         var removedLocalPlayer = _playerManager.GetPlayer(peer.Id, localUpdateMsg.LocalPlayerId);
-                        _playerManager.RemovePlayer(peer.Id, localUpdateMsg.LocalPlayerId);
+                        _playerManager.RemovePlayer(removedLocalPlayer);
                         _world.RemoveEntity(removedLocalPlayer);
+                        var writer = new NetDataWriter();
+                        writer.Put(new EntityRemovedMessage(removedLocalPlayer.Id));
                     }
+                }
                     break;
                 default:
                     Log.Warning("Unknown data message type received");
                     break;
+                
             }
+            
+        }
+
+        protected new void Exit()
+        {
+            _server.Stop();
+            base.Exit();
         }
     }
 }
