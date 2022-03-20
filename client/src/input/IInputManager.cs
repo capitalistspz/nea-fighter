7a73fe8d21984e8f6387eecd6c7e6010c4348619
capitalistspz <amponsahperez@gmail.com>
2022-03-20 20:37:46 +0000

    AimDirection removed from IInputManager

--- client/src/input/IInputManager.cs
+++ client/src/input/IInputManager.cs
@@ -6,7 +6,6 @@ namespace client.input
     // Base for all input managers
     public interface IInputManager<T>
     {
-        public Vector2 AimDirection { get; }
         // The thing controlled controlled by the input
         public T Owner { get; set; }
         public void Update(GameTime gameTime);
